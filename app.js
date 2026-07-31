1274b57aa090d77673523b22100126ebf153b24a
Laura <lauraguti@gmail.com>
2026-07-31 11:31:29 -0500

    configuracion dde variables de entorno y parametros

--- app.js
+++ app.js
@@ -13,6 +13,32 @@ app.get("/", (_, res) => {
 })
 
 //otro endpoint 
+app.get("/otraruta", (req, res)=>{
+    //usando template string
+    res.send(`<h1>Otro ejemplo de ruta</h1>
+        <h2>End point con res.send</h2>`)
+})
+
+
+app.get("/ruta2", (req, res)=>{
+    res.json({"Nombre": "Laura", "Apellido": "Gutierrez", "Cargo": "Aprendiz"})
+})
+
+app.get("/ruta3/:aprendiz/:otrodato", (req, res)=>{
+    const dato_aprendiz = req.params.aprendiz
+    const otro_dato = req.params.otrodato
+    res.json({"Nombre": dato_aprendiz, "Otro": otro_dato})
+})
+
+
+app.get("/ruta4", (req, res)=>{
+    //capturar el parametro de consulta query
+    const orden = req.query.orden || "sin ordenar"
+    const pagina = req.query.pagina || 1
+    res.send(`<h1>Listado Aprendices</h1>
+        <p>El listado esta en orden ${orden}</p>
+        <p>Pagina; ${pagina} </p>`)
+})
 
 app.listen(port, () => {
      console.log( `Servidor en funcionamiento en el puerto: ${port} http://localhost:3030`); 
