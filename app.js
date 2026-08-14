0f9437f75f95d82dc21a4cb3da3785aa031c70be
Laura <lauraguti@gmail.com>
2026-08-14 08:17:42 -0500

    terminado el punto 5(los scripts)

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express'); 
 const app = express();  
-const port = 3000; 
+const port = 3030; 
 
 app.get("/", (_, res) => {
     res.send("Aprendiendo express,ficha 3407181");
 });
 
 app.listen(port, () => {
-     console.log( `Servidor en funcionamiento en el puerto: `); 
+     console.log( `SERVIDOR: http://localhost:${port}`); 
 }); 
