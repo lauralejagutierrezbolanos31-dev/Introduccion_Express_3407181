7937a10645ff065c0a5faed3d078c0c46092b599
laura Gutierrez <lauragutierrez31@gmail.com>
2026-08-28 11:52:08 -0500

    enpoint crear nuevas colecciones para aprendiz

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express');
 const app = express();  
 require("dotenv/config")
 const puerto = process.env.PUERTO || 3000
+//configursr para la lecturs del archivo
 const sistemaArchivo = require("fs")
 const ruta = require("path")
 const rutaArchivoJson = ruta.join(__dirname, "datos.json")
 
-
+//enpoint raiz
 app.get("/", (_, res) => {
     res.send("Hola aprendiendo express");
 });
 
+//enpoint para ver los datos del archivo
 app.get("/api/aprendiz", (req, res)=>{
+    //datos vienen del archivo
     sistemaArchivo.readFile(rutaArchivoJson, "utf-8", (error, datos)=>{
         if(error){
             return res.json({Error:"no se puede leer los datos."})
@@ -20,6 +23,12 @@ app.get("/api/aprendiz", (req, res)=>{
         res.json(listaAprendices)
     })
 })
+
+//endpoint para crear aprendices 
+app.post("/api/aprendices", (req,res)=>{
+    res.json({Mensaje: "Trabajando en el endpoint"})
+})
+
 
 app.listen(puerto, function () {
      console.log( `SERVIDOR http://localhost:${puerto}`) 
